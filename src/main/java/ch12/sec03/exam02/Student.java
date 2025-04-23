4ebb20da43a25dd780b0ee67babcbef35eb7372a
hyunji <dlakgjl55@naver.com>
2025-04-23 16:04:21 +0900

    20250423 기본 문제

---
+++ src/main/java/ch12/sec03/exam02/Student.java
@@ -0,0 +1,7 @@
+package ch12.sec03.exam02;
+
+public class Student {
+    private int no;
+    private String name;
+
+}
---
+++ src/main/java/ch12/sec04/GetPropertyExample.java
@@ -0,0 +1,7 @@
+package ch12.sec04;
+import java.util.Properties;
+import java.util.Set;
+public class GetPropertyExample {
+    public static void main(String[] args) {
+    }
+}
